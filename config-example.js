d024eb703ce148698e36c181296cfeaee9cc80ca
Michael Enger <michaelenger@live.com>
2015-10-23 20:56:15 +0100

    Conform config file to code style
    Having mixed spaces and tabs and ' and " is ugly :/

--- config-example.js
+++ config-example.js
@@ -1,34 +1,28 @@
-var env = {
-    dev: 'dev',
-    test: 'test',
-    prod: 'prod'
-};
-
 module.exports = {
-    "appsettings" : {
-        "env" : env.dev
-    },
-    "email": {
-        "gmail": {
-            "user" : "",
-            "password" : ""
-        }
-    },
-    "giant_bomb": {
-        "url" : "http://www.giantbomb.com/api/",
-        "key" : "<Your API Key>"
-    },
-    "facebook" : {
-        "appid" : "<Your Facebook App Id>",
-        "appsecret" : "<Your Facebook App Secret>"
-    },
-    "steam" : {
-        "api_url" : 'http://api.steampowered.com/',
-        "api_key" : '<Your Steam API Key>',
-        "provider" : 'http://steamcommunity.com/openid'
-    },
-    "firebase" : {
-        "url" : '<Your Firebase Url>',
-        "secret" : '<Your Firebase Key>'
-    }
-}
+	appsettings: {
+		env: 'dev' // possible values: dev, test, prod
+	},
+	email: {
+		gmail: {
+			user: '',
+			password: ''
+		}
+	},
+	giant_bomb: {
+		url: 'http://www.giantbomb.com/api/',
+		key: '<Your API Key>'
+	},
+	facebook: {
+		appid: '<Your Facebook App Id>',
+		appsecret: '<Your Facebook App Secret>'
+	},
+	steam: {
+		api_url: 'http://api.steampowered.com/',
+		api_key: '<Your Steam API Key>',
+		provider: 'http://steamcommunity.com/openid'
+	},
+	firebase: {
+		url: '<Your Firebase Url>',
+		secret: '<Your Firebase Key>'
+	}
+};
